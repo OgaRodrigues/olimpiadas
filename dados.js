c0a6749dbca0f08b638575e83f88b988f1f336b7
OgaRodrigues <ogarodrigues@gmail.com>
2024-09-08 14:23:02 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao: "Raissa Leal é uma skatista brasileira que, com apenas 13 anos, conquistou a prata na estreia do skate street nas Olimpíadas de Tóquio 2020.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags:"skate sk8 fada"
+    }
+    
+    ,{
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou a medalha de prata no individual geral feminino nos Jogos Olímpicos de Tóquio 2020.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+
+    }
+    
+    ,{
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz conquistou a primeira medalha de ouro do Brasil nas Olimpíadas de 2024 em Paris e com isso se tornou a primeira mulher a conquistar uma medalha de ouro Olímpica na história do Esporte Clube Pinheiros.Na final da categoria acima de 78 kg, derrotou a israelense Raz Hershko. A vitória marcou um momento histórico para o judô brasileiro e se tornou a primeira medalha de ouro olímpica desde Rafaela Silva em 2016.", // Complete com a descrição da Beatriz
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza", 
+        tags: "Judoka judô judoca judo"
+    }
+];
+
